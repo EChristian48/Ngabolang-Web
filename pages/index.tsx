0d0cd9788a10fc1f85d1d80c3e24c12b7c91119b
Erick Christian <erickchristian48@gmail.com>
2020-11-04 21:18:43 +0700

    Add some memes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
 import Navbar from '@root/components/Navbar'
 
+type Img = {
+  url: string
+  location: string
+}
+
+const images: Img[] = [
+  { url: '/memes/wikrama.jpeg', location: 'Wikrama' },
+  { url: '/memes/me-using-dart.jpeg', location: 'Rumah' },
+  { url: '/memes/pain.jpeg', location: 'Internet' },
+  { url: '/memes/stackoverflow.jpeg', location: 'Internet' },
+  { url: '/memes/pikachu.jpg', location: 'Wikrama' },
+  { url: '/memes/ha.png', location: 'Rumah' },
+  { url: '/memes/handshake.jpg', location: 'Wikrama' },
+  { url: '/memes/java-py.png', location: 'Wikrama' },
+  { url: '/memes/hecker.jpg', location: 'Rumah' },
+]
 
 const Home = () => {
   return (
