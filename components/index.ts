5f81b31526a2088d357d6f10ff7e39f29dc1d39d
Erick Christian <erickchristian48@gmail.com>
2020-11-06 16:18:26 +0700

    Add Navbar and Layout to named imports

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
 export { default as LinkWrapper } from './LinkWrapper'
 export { default as EmailInput } from './EmailInput'
 export { default as PasswordInput } from './PasswordInput'
+export { default as Navbar } from './Navbar'
+export { default as Layout } from './Layout'
