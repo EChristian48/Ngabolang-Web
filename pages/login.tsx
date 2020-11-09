fa367ee2e64132e28d8416daa2c97db2de73033a
Erick Christian <erickchristian48@gmail.com>
2020-11-09 22:23:08 +0700

    Done login and register page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,53 +1,91 @@
-import { Button, Flex, Text } from '@chakra-ui/core'
-import { EmailInput, LinkWrapper, PasswordInput } from '@root/components'
+import { Button, Flex, Text, useToast } from '@chakra-ui/core'
+import { firebase } from '@firebase/client'
+import {
+  EmailInput,
+  LinkWrapper,
+  MustBeSignedOut,
+  PasswordInput,
+} from '@root/components'
+import useControlledInput from '@root/hooks/useControlledInput'
 import useLoading from '@root/hooks/useLoading'
 import classes from '@styles/Login.module.css'
+import 'firebase/auth'
 import { NextPage } from 'next'
+import { FormEvent } from 'react'
 import { FaGoogle } from 'react-icons/fa'
 
 const Login: NextPage = () => {
-  const { isLoading, startLoading } = useLoading(false)
+  const { isLoading, startLoading, stopLoading } = useLoading(false)
+  const [emailValue, emailHandler] = useControlledInput()
+  const [passValue, passHandler] = useControlledInput()
+
+  const failedToast = useToast()
+
+  async function login(method: Promise<unknown>) {
+    startLoading()
+    try {
+      await method
+    } catch (e) {
+      failedToast({
+        title: 'Failed to login',
+        description: (e as firebase.auth.AuthError).message,
+        isClosable: true,
+        status: 'error',
+      })
+    } finally {
+      stopLoading()
+    }
+  }
+
+  const loginWithEmail = (e: FormEvent<HTMLFormElement>) =>
+    login(firebase.auth().signInWithEmailAndPassword(emailValue, passValue))
+
+  const loginWithGoogle = () =>
+    login(
+      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+    )
 
   return (
-    <Flex alignItems='center' justifyContent='center' height='100vh'>
-      <form className={classes.loginForm} onSubmit={e => e.preventDefault()}>
-        <Text fontSize='2xl' marginBottom='2'>
-          Sign In
-        </Text>
+    <MustBeSignedOut>
+      <Flex alignItems='center' justifyContent='center' height='100vh'>
+        <form className={classes.loginForm} onSubmit={loginWithEmail}>
+          <Text fontSize='2xl' marginBottom='2'>
+            Sign In
+          </Text>
 
-        <EmailInput />
-        <PasswordInput />
+          <EmailInput onChange={emailHandler} value={emailValue} isRequired />
+          <PasswordInput onChange={passHandler} value={passValue} isRequired />
 
-        <Button
-          isLoading={isLoading}
-          width='full'
-          marginTop='2'
-          type='submit'
-          onClick={startLoading}
-        >
-          Login
-        </Button>
+          <Button
+            isLoading={isLoading}
+            width='full'
+            marginTop='2'
+            type='submit'
+          >
+            Login
+          </Button>
 
-        <Text fontSize='lg' marginY='2' marginTop='6'>
-          Or Sign In with
-        </Text>
-        <Button
-          isLoading={isLoading}
-          backgroundColor='blue.500'
-          color='white'
-          width='full'
-          leftIcon={FaGoogle}
-          onClick={startLoading}
-        >
-          Google
-        </Button>
+          <Text fontSize='lg' marginY='2' marginTop='6'>
+            Or Sign In with
+          </Text>
+          <Button
+            isLoading={isLoading}
+            backgroundColor='blue.500'
+            color='white'
+            width='full'
+            leftIcon={FaGoogle}
+            onClick={loginWithGoogle}
+          >
+            Google
+          </Button>
 
-        <Text marginTop='6' color='blue.600'>
-          Or{' '}
-          <LinkWrapper nextProps={{ href: '/register' }}>Sign Up</LinkWrapper>
-        </Text>
-      </form>
-    </Flex>
+          <Text marginTop='6' color='blue.600'>
+            Or{' '}
+            <LinkWrapper nextProps={{ href: '/register' }}>Sign Up</LinkWrapper>
+          </Text>
+        </form>
+      </Flex>
+    </MustBeSignedOut>
   )
 }
 
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,34 +1,84 @@
-import { Button, Flex, Text } from '@chakra-ui/core'
-import { EmailInput, PasswordInput } from '@root/components'
+import { Button, Flex, Text, useToast } from '@chakra-ui/core'
+import { EmailInput, MustBeSignedOut, PasswordInput } from '@root/components'
 import useLoading from '@root/hooks/useLoading'
 import classes from '@styles/Login.module.css'
 import { NextPage } from 'next'
+import { FormEvent } from 'react'
+import { firebase } from '@firebase/client'
+import 'firebase/auth'
+import useControlledInput from '@root/hooks/useControlledInput'
 
 const Register: NextPage = () => {
-  const { isLoading, startLoading } = useLoading(false)
+  const { isLoading, startLoading, stopLoading } = useLoading(false)
+  const [emailValue, emailHandler] = useControlledInput()
+  const [passValue, passHandler] = useControlledInput()
+  const [confPassValue, confPassHandler] = useControlledInput()
+
+  const failedToast = useToast()
+
+  async function signUp(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    startLoading()
+
+    if (passValue !== confPassValue) {
+      failedToast({
+        title: 'Failed to sign up',
+        description: "Those passwords don't match, duh",
+        isClosable: true,
+        status: 'warning',
+      })
+      return stopLoading()
+    }
+
+    try {
+      await firebase
+        .auth()
+        .createUserWithEmailAndPassword(emailValue, passValue)
+    } catch (e) {
+      failedToast({
+        title: 'Failed to sign up',
+        description: (e as firebase.auth.AuthError).message,
+        isClosable: true,
+        status: 'error',
+      })
+    } finally {
+      stopLoading()
+    }
+  }
 
   return (
-    <Flex alignItems='center' justifyContent='center' height='100vh'>
-      <form className={classes.loginForm}>
-        <Text fontSize='2xl' marginBottom='2'>
-          Register
-        </Text>
+    <MustBeSignedOut>
+      <Flex
+        alignItems='center'
+        justifyContent='center'
+        height='100vh'
+        onSubmit={signUp}
+      >
+        <form className={classes.loginForm}>
+          <Text fontSize='2xl' marginBottom='2'>
+            Register
+          </Text>
 
-        <EmailInput />
-        <PasswordInput />
-        <PasswordInput placeholder='Confirm Password' />
+          <EmailInput isRequired value={emailValue} onChange={emailHandler} />
+          <PasswordInput isRequired value={passValue} onChange={passHandler} />
+          <PasswordInput
+            isRequired
+            placeholder='Confirm Password'
+            value={confPassValue}
+            onChange={confPassHandler}
+          />
 
-        <Button
-          width='full'
-          marginTop='2'
-          type='submit'
-          isLoading={isLoading}
-          onClick={startLoading}
-        >
-          Register
-        </Button>
-      </form>
-    </Flex>
+          <Button
+            width='full'
+            marginTop='2'
+            type='submit'
+            isLoading={isLoading}
+          >
+            Register
+          </Button>
+        </form>
+      </Flex>
+    </MustBeSignedOut>
   )
 }
 
