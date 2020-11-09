ee6683ddc551d5eef174a093d9b6e40584d9eae1
Erick Christian <erickchristian48@gmail.com>
2020-11-09 22:22:16 +0700

    Add marginRight

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/core'
 import { FC } from 'react'
 
 const Layout: FC = ({ children }) => (
-  <Box marginLeft={[0, 0, 24, 24]} marginTop={[0, 0, 16, 16]}>{children}</Box>
+  <Box marginLeft={[2, 2, 24, 24]} marginTop={[0, 0, 16, 16]} marginRight={[2, 2, 2, 2]}>{children}</Box>
 )
 
 export default Layout
