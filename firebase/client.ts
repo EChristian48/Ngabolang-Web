5def83c241466fa53daea101c7786e3c7a89f574
Erick Christian <erickchristian48@gmail.com>
2020-11-09 22:23:36 +0700

    Added firebase initialization

---
+++ firebase/client.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAN0T-MJz1xoinsbuldmoMHfCEU0WcPgDY',
+    authDomain: 'ngabolang.firebaseapp.com',
+    databaseURL: 'https://ngabolang.firebaseio.com',
+    projectId: 'ngabolang',
+    storageBucket: 'ngabolang.appspot.com',
+    messagingSenderId: '205657255677',
+    appId: '1:205657255677:web:4bc497cc6ed268c128d894',
+    measurementId: 'G-GX4XWSKVQX',
+  })
+}
+
+export { firebase }
