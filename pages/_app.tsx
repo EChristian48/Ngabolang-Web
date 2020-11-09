5e615309899d32e29e918f8bc7fca978da9979b1
Erick Christian <erickchristian48@gmail.com>
2020-11-09 22:22:47 +0700

    Reorganized imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider, CSSReset } from '@chakra-ui/core'
-import { RecoilRoot } from 'recoil'
-import { AppProps } from 'next/app'
+import { CSSReset, ThemeProvider } from '@chakra-ui/core'
 import theme from '@root/chakra.theme'
+import { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
