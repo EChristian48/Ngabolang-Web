97597563d459b0dab1deba37d5cb306d7a0ed0a2
Erick Christian <erickchristian48@gmail.com>
2020-11-21 16:22:49 +0700

    Redone upload page

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,157 +1,162 @@
-// import {
-//   Button,
-//   Flex,
-//   Image,
-//   Input,
-//   Progress,
-//   Text,
-//   useToast,
-// } from '@chakra-ui/core'
-// import { Layout } from '@root/components'
-// import NeedAuth from '@root/components/NeedAuth'
-// import { Post } from '@root/data/types'
-// import useControlledInput from '@root/hooks/useControlledInput'
-// import { useDrop } from 'ahooks'
-// import firebase from 'firebase'
-// import 'firebase/firestore'
-// import 'firebase/storage'
-// import { NextPage } from 'next'
-// import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react'
+import {
+  Button,
+  Container,
+  Flex,
+  Image,
+  Input,
+  Progress,
+  SimpleGrid,
+  Text,
+  useToast,
+  VStack,
+} from '@chakra-ui/react'
+import Nav from '@root/components/Nav'
+import NeedAuth from '@root/components/NeedAuth'
+import { Post } from '@root/data/types'
+import { useDrop } from 'ahooks'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/storage'
+import { useInputHandler } from 'molohooks'
+import { NextPage } from 'next'
+import { ChangeEvent, FormEvent, useEffect, useRef, useState } from 'react'
 
-// const Upload: NextPage = () => {
-//   const [file, setFile] = useState<File>(null)
-//   const [imgSrc, setImgSrc] = useState<string>()
-//   const [progress, setProgress] = useState(0)
-//   const [location, setLocation] = useControlledInput()
-//   const inputFileRef = useRef<HTMLInputElement>()
+const Upload: NextPage = () => {
+  const [file, setFile] = useState<File>(null)
+  const [imgSrc, setImgSrc] = useState<string>()
+  const [progress, setProgress] = useState(-1)
+  const [location, setLocation, clearLocation] = useInputHandler()
+  const inputFileRef = useRef<HTMLInputElement>()
 
-//   const [dropHandlers, { isHovering }] = useDrop({
-//     onFiles: files => setFile(files[0]),
-//   })
+  const [dropHandlers] = useDrop({
+    onFiles: files => setFile(files[0]),
+  })
 
-//   const inputHandler = (e: ChangeEvent<HTMLInputElement>) =>
-//     setFile(e.target.files[0])
+  const inputHandler = (e: ChangeEvent<HTMLInputElement>) =>
+    setFile(e.target.files[0])
 
-//   useEffect(
-//     () => (file ? setImgSrc(URL.createObjectURL(file)) : setImgSrc('')),
-//     [file]
-//   )
+  useEffect(
+    () => (file ? setImgSrc(URL.createObjectURL(file)) : setImgSrc('')),
+    [file]
+  )
 
-//   const toast = useToast()
+  const toast = useToast()
 
-//   function uploadImage(e: FormEvent<HTMLFormElement>) {
-//     e.preventDefault()
+  function uploadImage(e: FormEvent<HTMLDivElement>) {
+    e.preventDefault()
 
-//     const { displayName, uid } = firebase.auth().currentUser
+    const { uid } = firebase.auth().currentUser
 
-//     const task = firebase
-//       .storage()
-//       .ref()
-//       .child(`posts/${uid}-${file.name}-${Date.now()}`)
-//       .put(file)
+    const task = firebase
+      .storage()
+      .ref()
+      .child(`posts/${uid}-${file.name}-${Date.now()}`)
+      .put(file)
 
-//     function observer(snapshot: firebase.storage.UploadTaskSnapshot) {
-//       setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
-//     }
+    function observer(snapshot: firebase.storage.UploadTaskSnapshot) {
+      setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+    }
 
-//     function onError(error: firebase.storage.FirebaseStorageError) {
-//       toast({
-//         title: 'Error uploading file',
-//         description: error.message,
-//         isClosable: true,
-//         status: 'error',
-//       })
-//     }
+    function onError(error: firebase.storage.FirebaseStorageError) {
+      toast({
+        title: 'Error uploading file',
+        description: error.message,
+        isClosable: true,
+        status: 'error',
+      })
+    }
 
-//     async function onDone() {
-//       const downloadUrl = await task.snapshot.ref.getDownloadURL()
+    async function onDone() {
+      const downloadUrl = await task.snapshot.ref.getDownloadURL()
 
-//       const post: Post = {
-//         displayName,
-//         uid,
-//         location,
-//         url: downloadUrl,
-//       }
+      const post: Post = {
+        uid,
+        location,
+        url: downloadUrl,
+        date: firebase.firestore.FieldValue.serverTimestamp(),
+      }
 
-//       await firebase.firestore().collection('posts').add(post)
+      await firebase.firestore().collection('posts').add(post)
 
-//       toast({
-//         title: 'Upload success',
-//         isClosable: true,
-//         status: 'success',
-//       })
-//       setFile(null)
-//       inputFileRef.current.value = ''
-//       setProgress(0)
-//     }
-//     task.on(firebase.storage.TaskEvent.STATE_CHANGED, observer, onError, onDone)
-//   }
+      toast({
+        title: 'Upload success',
+        isClosable: true,
+        status: 'success',
+      })
 
-//   return (
-//     <NeedAuth>
-//       <Layout>
-//         <Flex
-//           as='form'
-//           width='full'
-//           direction='column'
-//           align='center'
-//           onSubmit={uploadImage}
-//         >
-//           <Flex
-//             height='200px'
-//             width='70%'
-//             backgroundColor='red.300'
-//             rounded='7px'
-//             justify='center'
-//             align='center'
-//             direction='column'
-//             {...dropHandlers}
-//           >
-//             <Text color='white'>Drop your file here</Text>
+      clearLocation()
+      setFile(null)
+      inputFileRef.current.value = ''
+      setProgress(-1)
+    }
+    task.on(firebase.storage.TaskEvent.STATE_CHANGED, observer, onError, onDone)
+  }
 
-//             <Text>Or</Text>
+  return (
+    <NeedAuth>
+      <Nav />
 
-//             <Input
-//               type='file'
-//               maxWidth='250px'
-//               onChange={inputHandler}
-//               ref={inputFileRef}
-//               backgroundColor='red.300'
-//               border='none'
-//             />
-//           </Flex>
+      <Container maxWidth={['100%', , '80%']} marginTop={4}>
+        <SimpleGrid columns={[1, , 2]} spacing={4}>
+          <Flex
+            height={['300px', , '400px']}
+            borderWidth='4px'
+            borderStyle='dashed'
+            rounded='7px'
+            justify='center'
+            align='center'
+            direction='column'
+            {...dropHandlers}
+          >
+            <Text>Drop your file here</Text>
 
-//           {progress > 0 && (
-//             <Progress
-//               value={progress}
-//               size='lg'
-//               isAnimated
-//               hasStripe
-//               color='green'
-//               width='70%'
-//               marginTop='1'
-//             />
-//           )}
+            <Text>Or</Text>
 
-//           {imgSrc && (
-//             <>
-//               <Image alt='Selected image' src={imgSrc} height='200px' />
-//               <Input
-//                 placeholder='Location...'
-//                 value={location}
-//                 onChange={setLocation}
-//               />
-//               <Button type='submit'>Upload</Button>
-//             </>
-//           )}
-//         </Flex>
-//       </Layout>
-//     </NeedAuth>
-//   )
-// }
+            <label>
+              <input
+                type='file'
+                onChange={inputHandler}
+                ref={inputFileRef}
+                style={{ display: 'none' }}
+              />
+              <Button as='span'>Select Image</Button>
+            </label>
+          </Flex>
 
-// export default Upload
+          <VStack spacing={2} as='form' onSubmit={uploadImage}>
+            {imgSrc && (
+              <>
+                <Input
+                  placeholder='Tambah Lokasi...'
+                  value={location}
+                  size='lg'
+                  onChange={setLocation}
+                />
 
-export default function tolo() {
-  return <div>div</div>
+                <Button type='submit' isFullWidth>
+                  Upload
+                </Button>
+
+                {progress >= 0 && (
+                  <Progress
+                    value={progress}
+                    size='lg'
+                    isAnimated
+                    hasStripe
+                    color='green'
+                    width='100%'
+                    marginTop='1'
+                  />
+                )}
+
+                <Image alt='Selected image' src={imgSrc} maxWidth='400px' />
+              </>
+            )}
+          </VStack>
+        </SimpleGrid>
+      </Container>
+    </NeedAuth>
+  )
 }
+
+export default Upload
