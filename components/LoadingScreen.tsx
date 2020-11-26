99da5bee07d8f1dcbafc3bb9d43c3c8912f2ef69
Erick Christian <erickchristian48@gmail.com>
2020-11-26 15:44:00 +0700

    Add splash text, change color

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,7 +1,21 @@
-import { Flex, Spinner } from '@chakra-ui/react'
-import { FC } from 'react'
+import { Flex, Spinner, Text } from '@chakra-ui/react'
+import { splashesText } from '@root/data/splashes'
+import { Container } from '@chakra-ui/react'
+import { FC, useEffect, useState } from 'react'
 
 const LoadingScreen: FC = ({ children }) => {
+  const [currentSplash, setSplash] = useState<string>()
+
+  function randomSplash() {
+    const selected = Math.round(Math.random() * (splashesText.length - 1))
+    return setSplash(splashesText[selected])
+  }
+
+  useEffect(() => {
+    const randomSplashes = setInterval(randomSplash, 2000)
+    return () => clearInterval(randomSplashes)
+  }, [])
+
   return (
     <Flex
       justifyContent='center'
@@ -9,9 +23,14 @@ const LoadingScreen: FC = ({ children }) => {
       height='100vh'
       margin='0'
       direction='column'
+      backgroundColor='blue.500'
     >
-      <Spinner size='xl' marginBottom='4' />
-      {children}
+      <Spinner size='xl' marginBottom='4' color='white' thickness='4px' />
+      <Container maxWidth={['100%', , '80%']}>
+        <Text color='white' fontSize='xl' textAlign='center'>
+          {children || currentSplash}
+        </Text>
+      </Container>
     </Flex>
   )
 }
